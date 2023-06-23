e148dd1fc492b2bf2a9ab0516e5b05d2d82cd6d8
denchikj <igoshevdenis9876@gmail.com>
2023-06-23 21:05:35 +0500

    BinarySearch

---
+++ Algorithms.BinarySearch.ConsoleTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using Algorithms.BinarySearch.Implementations;
+
+namespace Algorithms.BinarySearch.ConsoleTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = new[] { 2, 5, 6, 7, 12, 14, 17, 19, 25, 36, 43, 64, 67, 78, 79 };
+            Console.WriteLine(Implementations.BinarySearch.Search(n, 14));
+            Console.WriteLine(Implementations.BinarySearch.Search(n, 67));
+        }
+    }
+}
---
+++ Algorithms.BinarySearch/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithms.BinarySearch.Implementations
+{
+    public class BinarySearch
+    {
+
+        public static int Search(int[] input, int element)
+        {
+            return Search(input, 0, input.Length - 1, element);
+        }
+
+        private static int Search(int[] input, int v1, int v2, int element)
+        {
+            while (true)
+            {
+                if (v1 == v2) 
+                {
+                    if (input[v1] == element) return v1;
+                    return -1;
+                }
+
+                var mid = (v1 + v2) / 2;
+                if (input[mid] == element) return mid;
+
+                if (input[mid] > element)
+                {
+                    v2 = mid;
+                    continue;
+                }
+
+                v1 = mid + 1;
+            }
+        }
+    }
+}
