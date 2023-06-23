6c8e0fd4c6ffd1da835a9cc6b214c951ca3d7402
denchikj <igoshevdenis9876@gmail.com>
2023-06-23 21:14:05 +0500

    ReverseString

---
+++ Algorithms.ReversingString.ConsoleTest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Algorithms.ReversingString.ConsoleTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "jsdag dgpdg ioetg ipgh k";
+            Console.WriteLine(Implementations.ReversingString.ReverseString(str));
+        }
+    }
+}
