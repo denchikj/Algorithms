dccf914c91d767d2c237e76d6ae32073ad3eac46
denchikj <igoshevdenis9876@gmail.com>
2023-06-24 19:22:13 +0500

    SelectionSort

---
+++ Algorithms.SelectionSort/SelectionSort.cs
@@ -0,0 +1,26 @@
+﻿namespace Algorithms.SelectionSort.Implementations
+{
+    public class SelectionSort
+    {
+        public static int[] SortSelection(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int min = i;
+                int temp = array[i];
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                array[i] = array[min];
+                array[min] = temp;
+            }
+
+            return array;
+        }
+    }
+}
