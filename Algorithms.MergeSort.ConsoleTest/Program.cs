8e80c77750fdee7722e3fe4287dad036b252db20
denchikj <igoshevdenis9876@gmail.com>
2023-06-24 19:40:31 +0500

    MergeSort

---
+++ Algorithms.MergeSort.ConsoleTest/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Algorithms.MergeSort.ConsoleTest
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 5, 3, 9, 10, 15, 12, 6, 20, 199, -10 };
+            foreach (int i in Implementations.MergeSort.SortMerge(array))
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ Algorithms.MergeSort/MergeSort.cs
@@ -0,0 +1,81 @@
+﻿namespace Algorithms.MergeSort.Implementations
+{
+    public class MergeSort
+    {
+        public static int[] SortMerge(int[] array)
+        {
+            int[] left;
+            int[] right;
+            int[] result = new int[array.Length];
+
+            if (array.Length <= 1 ) return array;
+
+            int mid = array.Length / 2;
+            left = new int[mid];
+
+            if (array.Length % 2 == 0) 
+                right = new int[mid];
+            else
+                right = new int[mid + 1];
+
+            for (int i = 0; i < mid; i++)
+            {
+                left[i] = array[i];
+            }
+
+            int x = 0;
+            for (int i = mid; i < array.Length; i++)
+            {
+                right[x] = array[i];
+                x++;
+            }
+
+            left = SortMerge(left);
+            right = SortMerge(right);
+            result = Merge(left, right);
+
+            return result;
+        }
+
+        private static int[] Merge(int[] left, int[] right)
+        {
+            int resultLength = right.Length + left.Length;
+            int[] result = new int[resultLength];
+
+            int indexLeft = 0, indexRight = 0, indexResult = 0;
+
+            while (indexLeft < left.Length || indexRight < right.Length)
+            {
+                if (indexLeft < left.Length && indexRight < right.Length)
+                {
+                    if (left[indexLeft] <= right[indexRight])
+                    {
+                        result[indexResult] = left[indexLeft];
+                        indexLeft++;
+                        indexResult++;
+                    }
+                    else
+                    {
+                        result[indexResult] = right[indexRight];
+                        indexRight++;
+                        indexResult++;
+                    }
+                }
+                else if (indexLeft < left.Length)
+                {
+                    result[indexResult] = left[indexLeft];
+                    indexLeft++;
+                    indexResult++;
+                }
+                else if (indexRight < right.Length)
+                {
+                    result[indexResult] = right[indexRight];
+                    indexRight++;
+                    indexResult++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
